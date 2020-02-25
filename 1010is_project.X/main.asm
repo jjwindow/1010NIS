fee03d7d5b7dd47dad491d44be9a21421d650115
jjw3517 <jjw3517@skbl-422-003.ph.ic.ac.uk>
2020-02-25 18:12:15 +0000

    RACKET DRAWN, responds to button press. write_racket routines need to be modified to show ball inside racket zone. goal moving fixed, right goal broken. attampted fix.

--- 1010is_project.X/main.asm
+++ 1010is_project.X/main.asm
@@ -1,32 +1,56 @@
 #include p18f87k22.inc
   
-    extern LED_Setup,Output_GRB
-	
+    extern  LED_Setup,Output_GRB, Write_state, Game_setup, Calculate_state
+    extern  delay_256, frame_delay, GREEN_pixeldata, CLEAR_pixeldata
+    extern  leftracket_true, leftracket_false, rightracket_true, rightracket_false
+    global  reset_animation
+    
 rst	code	0x0000	; reset vector
 	goto	start
 	
-acs0	udata_acs
-numLEDs	    res 1
-	
+acs0	udata_acs 0x0
+loopcount res 1
+ 
+ 
 main	code
+;--------------------MAIN------------------------	
+	
 start	call	LED_Setup		; Sit in infinite loop
-	;call	Output_GRB
-	goto $
-	movlw	.40
-	movwf	numLEDs
-	call	data_write
+	call	Game_setup
+gameloop
+	call	Calculate_state
+	btfss	PORTJ, 1
+	BRA	P1_nopress
+	call	leftracket_true
+	BRA	output
+P1_nopress
+	call	leftracket_false
+	btfss	PORTJ, 2
+	bra	P2_nopress
+	call	rightracket_true
+	bra	output
+P2_nopress
+	call	rightracket_false
+	bra	output
+output
+	call	Write_state
 	call	Output_GRB
-	goto $
+	call	frame_delay
+	goto	gameloop
 	
-data_write
-	LFSR	FSR0, 0x100
-loop1	movlw	b'11110000'
-	movwf	POSTINC0 
-	decfsz	numLEDs
-	goto	loop1
-	return
-	
-	end
+reset_animation
+	movlw	.6
+	movwf	loopcount
+loop1	call	CLEAR_pixeldata ; OUTPUT CLEAN
+	call	Output_GRB
+	call	frame_delay
+	call	GREEN_pixeldata	; OUTPUT GREEN
+	call	Output_GRB
+	call	frame_delay
+	decfsz	loopcount
+	goto	loop1		; and LOOP
+	return			 ;once looped enough times, return
+    end
 
 
 
